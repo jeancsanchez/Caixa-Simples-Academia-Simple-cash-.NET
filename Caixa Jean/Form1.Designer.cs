d119315cdee512ec09a52541cc2338c907b00270
Jean Carlos <jeancs.patricio@gmail.com>
2015-02-22 21:26:06 -0300

    Simple sistem cash, using C# (Training)

---
+++ Caixa Jean/Form1.Designer.cs
@@ -0,0 +1,185 @@
+﻿namespace Caixa_Jean
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.txtProduto = new System.Windows.Forms.TextBox();
+            this.lblNome = new System.Windows.Forms.Label();
+            this.listPodutos = new System.Windows.Forms.ListBox();
+            this.txtTotal = new System.Windows.Forms.TextBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.btnOK = new System.Windows.Forms.Button();
+            this.button1 = new System.Windows.Forms.Button();
+            this.pictureBox1 = new System.Windows.Forms.PictureBox();
+            this.label2 = new System.Windows.Forms.Label();
+            this.listInfo = new System.Windows.Forms.ListBox();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // txtProduto
+            // 
+            this.txtProduto.CharacterCasing = System.Windows.Forms.CharacterCasing.Upper;
+            this.txtProduto.Location = new System.Drawing.Point(14, 48);
+            this.txtProduto.Name = "txtProduto";
+            this.txtProduto.Size = new System.Drawing.Size(199, 20);
+            this.txtProduto.TabIndex = 0;
+            this.txtProduto.Enter += new System.EventHandler(this.txtProduto_Enter);
+            // 
+            // lblNome
+            // 
+            this.lblNome.AutoSize = true;
+            this.lblNome.Font = new System.Drawing.Font("Microsoft Sans Serif", 10.25F);
+            this.lblNome.Location = new System.Drawing.Point(14, 27);
+            this.lblNome.Name = "lblNome";
+            this.lblNome.Size = new System.Drawing.Size(118, 17);
+            this.lblNome.TabIndex = 1;
+            this.lblNome.Text = "Nome do produto";
+            // 
+            // listPodutos
+            // 
+            this.listPodutos.FormattingEnabled = true;
+            this.listPodutos.Location = new System.Drawing.Point(315, 79);
+            this.listPodutos.Name = "listPodutos";
+            this.listPodutos.ScrollAlwaysVisible = true;
+            this.listPodutos.Size = new System.Drawing.Size(305, 134);
+            this.listPodutos.TabIndex = 2;
+            // 
+            // txtTotal
+            // 
+            this.txtTotal.Font = new System.Drawing.Font("Microsoft Sans Serif", 10.25F);
+            this.txtTotal.Location = new System.Drawing.Point(508, 234);
+            this.txtTotal.Name = "txtTotal";
+            this.txtTotal.ReadOnly = true;
+            this.txtTotal.Size = new System.Drawing.Size(112, 23);
+            this.txtTotal.TabIndex = 3;
+            this.txtTotal.TextChanged += new System.EventHandler(this.txtTotal_TextChanged);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 10.25F);
+            this.label1.ForeColor = System.Drawing.Color.ForestGreen;
+            this.label1.Location = new System.Drawing.Point(444, 240);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(62, 17);
+            this.label1.TabIndex = 4;
+            this.label1.Text = "Total R$";
+            // 
+            // btnOK
+            // 
+            this.btnOK.Location = new System.Drawing.Point(574, 47);
+            this.btnOK.Name = "btnOK";
+            this.btnOK.Size = new System.Drawing.Size(46, 23);
+            this.btnOK.TabIndex = 5;
+            this.btnOK.Text = "Ok";
+            this.btnOK.UseVisualStyleBackColor = true;
+            this.btnOK.Click += new System.EventHandler(this.btnOK_Click);
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(225, 47);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(62, 23);
+            this.button1.TabIndex = 6;
+            this.button1.Text = "Pesquisar";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // pictureBox1
+            // 
+            this.pictureBox1.Location = new System.Drawing.Point(22, 81);
+            this.pictureBox1.Name = "pictureBox1";
+            this.pictureBox1.Size = new System.Drawing.Size(269, 170);
+            this.pictureBox1.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.pictureBox1.TabIndex = 7;
+            this.pictureBox1.TabStop = false;
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Font = new System.Drawing.Font("Microsoft Sans Serif", 10.25F);
+            this.label2.ForeColor = System.Drawing.SystemColors.HotTrack;
+            this.label2.Location = new System.Drawing.Point(315, 27);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(85, 17);
+            this.label2.TabIndex = 9;
+            this.label2.Text = "Informações";
+            // 
+            // listInfo
+            // 
+            this.listInfo.BackColor = System.Drawing.SystemColors.ButtonFace;
+            this.listInfo.ForeColor = System.Drawing.SystemColors.MenuHighlight;
+            this.listInfo.FormattingEnabled = true;
+            this.listInfo.Location = new System.Drawing.Point(315, 49);
+            this.listInfo.Name = "listInfo";
+            this.listInfo.Size = new System.Drawing.Size(253, 17);
+            this.listInfo.TabIndex = 10;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.AutoSize = true;
+            this.ClientSize = new System.Drawing.Size(633, 273);
+            this.Controls.Add(this.listInfo);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.pictureBox1);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.btnOK);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.txtTotal);
+            this.Controls.Add(this.listPodutos);
+            this.Controls.Add(this.lblNome);
+            this.Controls.Add(this.txtProduto);
+            this.MaximizeBox = false;
+            this.Name = "Form1";
+            this.Padding = new System.Windows.Forms.Padding(3);
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "frmPrincipal";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.TextBox txtProduto;
+        private System.Windows.Forms.Label lblNome;
+        private System.Windows.Forms.ListBox listPodutos;
+        private System.Windows.Forms.TextBox txtTotal;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Button btnOK;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.PictureBox pictureBox1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.ListBox listInfo;
+    }
+}
+
---
+++ Caixa Jean/Form1.cs
@@ -0,0 +1,109 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Caixa_Jean
+{
+    public partial class Form1 : Form
+    {
+       
+       //////////////////////////////////////////      
+        const int numeroDeProdutos = 8;        //
+       //////////////////////////////////////////
+      
+        int[] Id = new int[numeroDeProdutos];
+        string[] Nome = new string[numeroDeProdutos];
+        decimal[] Preco = new decimal[numeroDeProdutos];
+        decimal Total = 0;
+        int preco;
+        public Form1()
+        {
+            InitializeComponent();
+        }
+       
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            loadProdutos();
+        }
+        
+        public void loadProdutos()
+        {
+            Id[0] = 1;
+            Nome[0] = "WHEY PROTEIN";
+            Preco[0] = 200.00M;
+
+            Id[1] = 2;
+            Nome[1] = "YOGURT";
+            Preco[1] = 7.00M;
+
+            Id[2] = 3;
+            Nome[2] = "GRANOLA";
+            Preco[2] = 5.00M;
+
+            Id[3] = 4;
+            Nome[3] = "MEL KARO";
+            Preco[3] = 9.75M;
+
+            Id[4] = 5;
+            Nome[4] = "LEITE DESNATADO";
+            Preco[4] = 4.53M;
+
+            Id[5] = 6;
+            Nome[5] = "OVO";
+            Preco[5] = 0.35M;
+
+            Id[6] = 7;
+            Nome[6] = "ALBUMINA";
+            Preco[6] = 33.00M;
+
+            Id[7] = 8;
+            Nome[7] = "QUEIJO COALHO";
+            Preco[7] = 15.73M;
+        }
+
+       
+        private void button1_Click(object sender, EventArgs e)
+        {
+            int ind = -1;
+            listInfo.Items.Clear();
+            txtProduto.Text = "";
+            for (int i = 0; i < 8; i++)
+            {
+                if (txtProduto.Text == Nome[i])
+                {
+                    listInfo.Items.Add(String.Format("Id:{0}      {1}       R$ {2}", Id[i], Nome[i], Preco[i]));
+                    pictureBox1.ImageLocation = "C:/Users/JCNOT/Documents/Visual Studio 2013/Projects/Caixa Jean/Cash Imagens/" + Nome[i] + ".jpg";
+                    ind = 0;
+                    preco = i;
+                }
+            }
+            if (ind == -1)
+                MessageBox.Show("Produto não existe!");
+        }
+        private void btnOK_Click(object sender, EventArgs e)
+        {
+            listPodutos.Items.Add(String.Format("Id:{0}      {1}       R$ {2}", Id[preco], Nome[preco], Preco[preco]));
+            Total += Preco[preco];
+            txtTotal.Text = Convert.ToString(Total);
+        }
+        private void txtTotal_TextChanged(object sender, EventArgs e)
+        {
+        }
+
+        private void txtProduto_Enter(object sender, KeyPressEventArgs e)
+        {
+            if (e.KeyChar == 13)
+                this.button1_Click(sender, e);
+        }
+        private void txtProduto_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
